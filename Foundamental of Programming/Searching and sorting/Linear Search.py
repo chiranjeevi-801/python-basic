2b9310afcfbcd13943bf33a9a8d300c0c2dfeb9a
chiranjeevi-801 <chiranjeevichiranjeevi6786@gmail.com>
2025-09-12 19:36:20 +0530

    udpdate code

--- Foundamental of Programming/Searching and sorting/Linear Search.py
+++ Foundamental of Programming/Searching and sorting/Linear Search.py
@@ -1,3 +1,16 @@
+#How It Works:-----
+#Start from the first element of the list.
+#compare it with the target value.
+#If they are equal → ✅ return the index (position).
+#If not → move to the next element.
+#Keep doing this until you either:
+#Find the target → return its index.
+#Reach the end without finding it → return -1.
+
+
+
+
+
 def linear_search(arr,target):
     size=len(arr)
     for index in range(0,size):
@@ -11,4 +24,4 @@ target=20
 
 result=linear_search(my_list,target)
 
-print(result)
+print(result) 
